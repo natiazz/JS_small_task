531cbdd9733485ebd0e373383a5ff721bf95f129
natiazz <116018905+natiazz@users.noreply.github.com>
2022-12-18 20:36:48 +0400

    Add files via upload

---
+++ JS1.js
@@ -0,0 +1,20 @@
+// task 1
+
+let name = "Natia";
+
+let surename = "Avaliani";
+
+let sex = "Female";
+
+let BirthYear = "2001";
+
+// task 2
+
+// ( 100 - (10+15)/5)*3
+
+// there is 8 expresion in this code
+
+// task 3
+
+let number = 101%2;
+console.log(number); 
